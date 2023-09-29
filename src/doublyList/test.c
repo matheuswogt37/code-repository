0dcbcac4a5ab370be0daaf46a30281251cdb7404
matheuswogt37 <matheusnotas07@gmail.com>
2023-09-29 09:21:29 -0300

    doubly list add, test is ok

---
+++ src/doublyList/test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "doublyLinkedList.c"
+#define arrayLength(array) (int) (sizeof((array)) / sizeof((array)[0]))
+
+
+int main() {
+
+    int addEle[] = {1, 3, 5, 7, 1, 2};
+    List *queue;
+    queue = allocList();
+    int arrayLenght = arrayLength(addEle);
+
+    for (int i = 0; i < arrayLenght; i++) {
+        insertNodo(queue, queue->tail, addEle[i]);
+    }
+
+    printList(queue);
+    
+    printf("\n\ntamanho da lista: %i\n", queue->size);
+    printf("head: %i\n", queue->head->info);
+    printf("tail: %i\n", queue->tail->info);
+
+
+
+    return 0;
+}

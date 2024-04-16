004962a52d8163daee678c68add57b6d0a7712bb
matheuswogt37 <matheusnotas07@gmail.com>
2024-04-15 21:26:32 -0300

    remove work, except for source

--- src/C/utils/data-structures/binary-tree/basic-int-tree.c
+++ src/C/utils/data-structures/binary-tree/basic-int-tree.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct  sNode
+typedef struct sNode
 {
     int key;
     struct sNode *father;
@@ -9,15 +9,13 @@ typedef struct  sNode
     struct sNode *left;
 } Node;
 
-typedef struct 
+// utils for alloc and free memory
+Node *createNode(int info)
 {
-    Node *source;
-} Binary_tree;
-
-Node *createNode(int info) {
     Node *node;
-    node = (Node *) malloc(sizeof(Node));
-    if (node != NULL) {
+    node = (Node *)malloc(sizeof(Node));
+    if (node != NULL)
+    {
         node->key = info;
         node->father = NULL;
         node->left = NULL;
@@ -26,106 +24,287 @@ Node *createNode(int info) {
     return node;
 }
 
-Binary_tree *createBinaryTree() {
-    Binary_tree *tree;
-    tree = (Binary_tree *) malloc(sizeof(Binary_tree));
-    if (tree != NULL) {
-        tree->source = NULL;
+void freeTree(Node *node)
+{
+    if (node->left != NULL)
+    {
+        freeTree(node->left);
     }
-    return tree;
-}
-
-void freeNode(Node *node) {
-    if (node->left != NULL) {
-        freeNode(node->left);
-    }
-    if (node->right != NULL) {
-        freeNode(node->right);
+    if (node->right != NULL)
+    {
+        freeTree(node->right);
     }
     free(node);
 }
 
-void freeBinaryTree(Binary_tree *tree) {
-    if (tree->source != NULL) {
-        freeNode(tree->source);
+// search with recursive function
+Node *recSearchByInfo(Node *node, int info)
+{
+    if (node->key == info)
+    {
+        return node;
     }
-    free(tree);
+    if (node->right != NULL)
+    {
+        if (info > node->key)
+        {
+            return recSearchByInfo(node->right, info);
+        }
+    }
+    if (node->left != NULL)
+    {
+        return recSearchByInfo(node->left, info);
+    }
+    printf("Valor %d nao encontrado!\n", info);
+    return NULL;
 }
 
-void insertNodeInTree(Node *father, Node *node) {
-    if (node->key > father->key) {
-        if (father->right == NULL) {
+// search withour recursive
+Node *searchByInfo(Node *source, int info)
+{
+    Node *aux;
+    aux = source;
+    while (aux != NULL)
+    {
+        if (aux->key == info)
+        {
+            return aux;
+        }
+        if (info > aux->key)
+        {
+            aux = aux->right;
+        }
+        else
+        {
+            aux = aux->left;
+        }
+    }
+    printf("Valor %d nao encontrado!\n", info);
+    return aux;
+}
+
+// insert node
+void insertNodeByNode(Node *father, Node *node)
+{
+    if (node->key > father->key)
+    {
+        if (father->right == NULL)
+        {
             father->right = node;
             node->father = father;
-        } else {
-            insertNodeInTree(father->right, node);
         }
-    } else /* if (node->key < father->key) */ {
-        if (father->left == NULL) {
+        else
+        {
+            insertNodeByNode(father->right, node);
+        }
+    }
+    else /* if (node->key < father->key) */
+    {
+        if (father->left == NULL)
+        {
             father->left = node;
             node->father = father;
-        } else {
-            insertNodeInTree(father->left, node);
+        }
+        else
+        {
+            insertNodeByNode(father->left, node);
         }
     }
 }
 
-void insertNode(Binary_tree *tree, int info) {
-    Node *node = createNode(info);
-    if (tree->source == NULL) {
-        tree->source = node;
-    } else {
-        insertNodeInTree(tree->source, node);
+void insertNodeByInfo(Node *father, int info)
+{
+    Node *node;
+    node = createNode(info);
+    if (node != NULL)
+    {
+        insertNodeByNode(father, node);
     }
 }
 
+// minimum and maximum
+Node *getMinimum(Node *node)
+{
+    if (node->left == NULL)
+    {
+        return node;
+    }
+    return getMinimum(node->left);
+}
 
-void printInOrder(Node *node) {
-    if (node != NULL) {
+Node *getMaximum(Node *node)
+{
+    if (node->right == NULL)
+    {
+        return node;
+    }
+    return getMinimum(node->right);
+}
+
+// remove nodes
+void removeNodeByInfo(Node *source, int info)
+{
+    Node *remNode;
+    Node *aux = NULL;
+    remNode = searchByInfo(source, info);
+    if (remNode == NULL)
+    {
+        printf("Valor %d nao existe! Impossivel remover\n", info);
+    }
+    else
+    {
+        if (remNode->right == NULL && remNode->left != NULL)
+        {
+            aux = remNode->left;
+            aux->father = remNode->father;
+        }
+        else if (remNode->right != NULL && remNode->left == NULL)
+        {
+            aux = remNode->right;
+            aux->father = remNode->father;
+        }
+        else if (remNode->right != NULL && remNode->left != NULL)
+        {
+            aux = remNode->right;
+            aux->father = remNode->father;
+            if (remNode->left != NULL)
+            {
+                if (aux->left != NULL)
+                {
+                    insertNodeByNode(aux, remNode->left);
+                }
+                else
+                {
+                    aux->left = remNode->left;
+                    remNode->left->father = aux;
+                }
+            }
+        }
+
+        if (remNode->father != NULL)
+        {
+            if (remNode->father->right == remNode)
+            {
+                remNode->father->right = aux;
+            }
+            else
+            {
+                remNode->father->left = aux;
+            }
+        }
+
+        if (remNode == source)
+        {
+            source = aux;
+        }
+        free(remNode);
+    }
+}
+
+// transplant nodes
+void transplantNodesByNodes(Node *source, Node *nodeReplaced, Node *nodeSwitch)
+{
+    Node *aux = NULL;
+    if (nodeSwitch->right != NULL)
+    {
+        aux = nodeSwitch->right;
+    }
+    else if (nodeSwitch->left != NULL)
+    {
+        aux = nodeSwitch->right;
+    }
+
+
+    if (nodeSwitch->father != NULL) {
+        if (nodeSwitch->father->right == nodeSwitch) {
+            nodeSwitch->father->right = aux;
+        } else {
+            nodeSwitch->father->left = aux;
+        }
+    }
+
+    nodeReplaced->left = nodeSwitch->left;
+    nodeReplaced->right = nodeSwitch->right;
+    nodeReplaced->father = nodeSwitch->father;
+}
+
+void transplantNodesByInfo(Node *source, int nodeReplaced, int nodeSwitch)
+{
+    Node *nReplaced = searchByInfo(source, nodeReplaced);
+    Node *nSwitch = searchByInfo(source, nodeSwitch);
+    switchNodesByNodes(source, nReplaced, nodeSwitch);
+}
+
+// prints
+void printInOrder(Node *node)
+{
+    if (node != NULL)
+    {
         printInOrder(node->left);
         printf("%d ", node->key);
         printInOrder(node->right);
     }
 }
 
-void printPreOrder(Node *node) {
-    if (node != NULL) {
+void printPreOrder(Node *node)
+{
+    if (node != NULL)
+    {
         printf("%d ", node->key);
         printPreOrder(node->left);
         printPreOrder(node->right);
     }
 }
 
-void printPostOrder(Node *node) {
-    if (node != NULL) {
+void printPostOrder(Node *node)
+{
+    if (node != NULL)
+    {
         printPostOrder(node->left);
         printPostOrder(node->right);
         printf("%d ", node->key);
     }
 }
 
+int main()
+{
+    int test[] = {5, 9, 8, 4, 10};
 
-int main() {
-    int test[] = {6, 5, 7, 4, 8};
-    Binary_tree *tree = createBinaryTree();
+    Node *source = createNode(6);
 
-    for (int i = 0; i < 5; i++) {
-        insertNode(tree, test[i]);
+    for (int i = 0; i < 5; i++)
+    {
+        insertNodeByInfo(source, test[i]);
     }
 
     printf("inOrder\n");
-    printInOrder(tree->source);
+    printInOrder(source);
     printf("\n");
-    
-    printf("preOrder\n");
-    printPreOrder(tree->source);
-    printf("\n");
-    
-    printf("postOrder\n");
-    printPostOrder(tree->source);
-    printf("\n");
-    
 
-    freeBinaryTree(tree);
+    removeNodeByInfo(source, 6);
+
+    printf("source -> %d\n", source->key);
+
+    printf("inOrder\n");
+    printInOrder(source);
+    printf("\n");
+
+    // TEST - search tree key
+    // searchTreeKey(tree, 8);
+
+    // TEST - prints
+    // printf("inOrder\n");
+    // printInOrder(source);
+    // printf("\n");
+
+    // printf("preOrder\n");
+    // printPreOrder(source);
+    // printf("\n");
+
+    // printf("postOrder\n");
+    // printPostOrder(source);
+    // printf("\n");
+
+    freeTree(source);
     return 0;
 }

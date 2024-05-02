5e98e9bfe44e242cc3b4a1f887ed5b222f98eb9f
matheuswogt37 <matheusnotas07@gmail.com>
2024-05-02 19:50:11 -0300

    avl tree work

--- src/C/utils/data-structures/binary-tree/binary-tree.int.c
+++ src/C/utils/data-structures/binary-tree/binary-tree.int.c
@@ -91,6 +91,205 @@ Node *getMaximum(Node *source)
     return getMaximum(source->right);
 }
 
+Node *findFather(Node *source, Node *node)
+{
+    if (source == NULL || source->left == node || source->right == node)
+    {
+        return source;
+    }
+    if (node->key > source->key)
+    {
+        return findFather(source->right, node);
+    }
+    else
+    {
+        return findFather(source->left, node);
+    }
+}
+
+// AVL
+
+// right rotation
+Node *rightRotation(Node **realSource, Node *oldNode, Node *newNode)
+{
+    Node *father = findFather(*realSource, oldNode);
+    if (father != NULL) // oldNode->father->child = newNode
+    {
+        if (father->left = oldNode)
+        {
+            father->left = newNode;
+        }
+        else
+        {
+            father->right = newNode;
+        }
+    }
+
+    oldNode->right = newNode->left;
+    newNode->left = oldNode;
+
+    if (oldNode == *realSource)
+    {
+        *realSource = newNode;
+    }
+    return *realSource;
+}
+
+// left rotation
+Node *leftRotation(Node **realSource, Node *oldNode, Node *newNode)
+{
+    Node *father = findFather(*realSource, oldNode);
+    if (father != NULL) // oldNode->father->child = newNode
+    {
+        if (father->left = oldNode)
+        {
+            father->left = newNode;
+        }
+        else
+        {
+            father->right = newNode;
+        }
+    }
+
+    oldNode->left = newNode->right;
+    newNode->right = oldNode;
+
+    if (oldNode == *realSource)
+    {
+        *realSource = newNode;
+    }
+    return *realSource;
+}
+// left left case
+void LLCase(Node **realSource, Node *oldNode, Node *newNode)
+{
+    leftRotation(realSource, oldNode, newNode);
+}
+
+// right right case
+void RRCase(Node **realSource, Node *oldNode, Node *newNode)
+{
+    rightRotation(realSource, oldNode, newNode);
+}
+
+// left right case
+void LRCase(Node **realSource, Node *oldNode, Node *newNode)
+{
+    leftRotation(realSource, newNode, newNode->right);
+    rightRotation(realSource, oldNode, oldNode->left);
+}
+
+// right left case
+void RLCase(Node **realSource, Node *oldNode, Node *newNode)
+{
+    rightRotation(realSource, newNode, newNode->left);
+    leftRotation(realSource, oldNode, oldNode->right);
+}
+
+// calc only height [return height, recursive function]
+int calcOnlyHeight(Node *source)
+{
+    if (source == NULL)
+    {
+        return 0;
+    }
+    int actualyHeight, tempHeight;
+    actualyHeight = tempHeight = 0;
+    if (source->left != NULL)
+    {
+        actualyHeight = calcOnlyHeight(source->left);
+    }
+    if (source->right != NULL)
+    {
+        tempHeight = calcOnlyHeight(source->right);
+    }
+    if (tempHeight > actualyHeight)
+    {
+        return tempHeight + 1;
+    }
+    return actualyHeight + 1;
+}
+
+// calc greater height [1 - left, 2 - right]
+int calcGreaterHeight(Node *source)
+{
+    int lHeight, rHeight;
+    lHeight = rHeight = 0;
+    if (source->left != NULL)
+    {
+        lHeight = calcOnlyHeight(source->left);
+    }
+    if (source->right != NULL)
+    {
+        rHeight = calcOnlyHeight(source->right);
+    }
+    if (lHeight > rHeight)
+    {
+        return 1;
+    }
+    return 2;
+}
+
+// calc balace factor
+
+void calcBalanceFactor(Node **RealSource, Node *source)
+{
+    int leftHeight, rightHeight, balanceFactor, tempLHeight, tempRHeight;
+    tempLHeight = tempRHeight = 0;
+
+    leftHeight = calcOnlyHeight(source->left);
+    rightHeight = calcOnlyHeight(source->right);
+    balanceFactor = leftHeight - rightHeight;
+    if (balanceFactor < -1)
+    {
+        if (source->right->left != NULL)
+        {
+            tempLHeight = calcOnlyHeight(source->right->left);
+        }
+        if (source->right->right != NULL)
+        {
+            tempRHeight = calcOnlyHeight(source->right->right);
+        }
+        if (tempLHeight > tempRHeight)
+        {
+            // case right - left
+            RLCase(RealSource, source, source->right);
+        }
+        else
+        {
+            // case right - right
+            RRCase(RealSource, source, source->right);
+        }
+    }
+    else if (balanceFactor > 1)
+    {
+        // when right - ? give priority to source->left greater height if (source->left->left != NULL) // calc souce->right->left height
+        {
+            tempLHeight = calcGreaterHeight(source->left->left);
+        }
+        if (source->left->right != NULL) // calc souce->right->right height
+        {
+            tempLHeight = calcGreaterHeight(source->left->right);
+        }
+        if (tempRHeight > tempLHeight)
+        {
+            // case left - right
+            LRCase(RealSource, source, source->left);
+        }
+        else
+        {
+            // case left - left
+            LLCase(RealSource, source, source->left);
+        }
+    }
+    if (source->left != NULL) {
+        calcBalanceFactor(RealSource, source->left);
+    }
+    if (source->right != NULL) {
+    calcBalanceFactor(RealSource, source->right);
+    }
+}
+
 // insert
 Node *insertNodeByInfo(Node *source, int info)
 {
@@ -107,20 +306,6 @@ Node *insertNodeByInfo(Node *source, int info)
         source->left = insertNodeByInfo(source->left, info);
     }
     return source;
-
-}
-
-// find father
-Node *findFather(Node *source, Node *node)
-{
-    if (source == NULL || source->left == node || source->right == node) {
-        return source;
-    }
-    if (node->key > source->key) {
-        return findFather(source->right, node);
-    } else {
-        return findFather(source->left, node);
-    }
 }
 
 // transplant
@@ -200,22 +385,52 @@ void printPostOrder(Node *node)
 
 int main()
 {
+
+    // int test[] = {5, 9, 8, 4, 10};
+
+    // Node *source = createNode(6);
+    // int *height = 0;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     source = insertNodeByInfo(source, test[i]);
+    // }
+
+    // printInOrder(source);
+    // printf("\n");
+
+    // // deleteNodeByNode(&source, searchNode(source, 6));
+
+    // insertNodeByInfo(source, 3);
+    // insertNodeByInfo(source, 2);
+    // insertNodeByInfo(source, 1);
+    // calcBalanceFactor(&source, source, height);
+
+    // printInOrder(source);
+    // printf("\n");
+
+    // TESTES DE FUNCOES
+
     int test[] = {5, 9, 8, 4, 10};
 
     Node *source = createNode(6);
+    int *height = 0;
 
     for (int i = 0; i < 5; i++)
     {
         source = insertNodeByInfo(source, test[i]);
     }
 
-    printInOrder(source);
-    printf("\n");   
+    source = insertNodeByInfo(source, 3);
+    source = insertNodeByInfo(source, 2);
+    source = insertNodeByInfo(source, 1);
+    printf("\nsource before calc- %i\n", source->key);
+    calcBalanceFactor(&source, source);
 
-    deleteNodeByNode(&source, searchNode(source, 6));
-
     printInOrder(source);
-    printf("\n");
+    printf("\nsource after calc- %i\n", source->key);
+
+    printf("terminado\n");
 
     return 0;
 }

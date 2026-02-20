5bfa444379fc1c6846cd9e53abaf9955cddda604
matheuswogt37 <matheusnotas07@gmail.com>
2026-02-20 14:14:21 -0300

    triangles movement is ok

--- src/C++/utils/glfw/src/main.cpp
+++ src/C++/utils/glfw/src/main.cpp
@@ -3,7 +3,8 @@
 #include <iostream>
 
 //* global vars
-float rotation = 0.0f;
+bool rotateLeft = false;
+bool rotateRight = false;
 
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
     
@@ -11,15 +12,15 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     } else if (key == GLFW_KEY_A) {
         if (action == GLFW_PRESS) {
-            rotation += 100.0f;
+            rotateLeft = true;
         } else if (action == GLFW_RELEASE) {
-            rotation -= 100.0f;
+            rotateLeft = false;
         }
     } else if (key == GLFW_KEY_D) {
         if (action == GLFW_PRESS) {
-            rotation -= 100.0f;
+            rotateRight = true;
         } else if (action == GLFW_RELEASE) {
-            rotation += 100.0f;
+            rotateRight = false;
         }
     }
 }
@@ -42,6 +43,8 @@ int main() {
 
     glfwSetKeyCallback(window, key_callback);
 
+    float lastTime = 0.0f;
+    float rotation = 0.0f;
     while(!glfwWindowShouldClose(window)) {
         float ratio;
         int width, height;
@@ -58,19 +61,24 @@ int main() {
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
 
-        float lastTime = 0.0f;
         float currentTime = glfwGetTime();
         float deltaTime = currentTime - lastTime;
         lastTime = currentTime;
 
+        if (rotateLeft) {
+            rotation += 100.0f * deltaTime;
+        }
+        if (rotateRight) {
+            rotation -= 100.0f * deltaTime;
+        }
 
-        glRotatef(rotation * deltaTime, 0.f, 0.f, 1.f);
+        glRotatef(rotation, 0.f, 0.f, 1.f);
         glBegin(GL_TRIANGLES);
-        glColor3f(0.f, 0.f, 0.f);
+        glColor3f(1.f, 0.f, 0.f);
         glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(1.f, 1.f, 1.f);
+        glColor3f(0.f, 1.f, 0.f);
         glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
         glVertex3f(0.f, 0.6f, 0.f);
         glEnd();
 

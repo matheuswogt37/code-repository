0f5987d4fd94b8aa105930a772e18aa6b5797bc6
matheuswogt37 <matheusnotas07@gmail.com>
2026-02-20 11:01:39 -0300

    show triangle

---
+++ src/C++/utils/glfw/src/main.cpp
@@ -0,0 +1,78 @@
+#include <GLFW/glfw3.h>
+#include "linmath.h"
+#include <iostream>
+
+//* global vars
+float rotation = 0.0f;
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    } else if (key == GLFW_KEY_A) {
+        if (action == GLFW_PRESS) {
+            rotation += 50.0f;
+        } else if (action == GLFW_RELEASE) {
+            rotation -= 50.0f;
+        }
+    } else if (key == GLFW_KEY_D) {
+        if (action == GLFW_PRESS) {
+            rotation -= 50.0f;
+        } else if (action == GLFW_RELEASE) {
+            rotation += 50.0f;
+        }
+    }
+}
+
+int main() {
+    
+    if(!glfwInit()) {
+        //! Error init glfw
+        return 1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(1, 1, "Hello World!", NULL, NULL);
+    if(!window) {
+        //! Error init window
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    while(!glfwWindowShouldClose(window)) {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
+        ratio = width / (float) height;
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+
+        
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glRotatef((float) glfwGetTime() * rotation, 0.f, 0.f, 1.f);
+        glBegin(GL_TRIANGLES);
+        glColor3f(0.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(1.f, 1.f, 1.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 0.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
